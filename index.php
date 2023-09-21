d3fcf3b3b7a419171b7d735bde024f32a3bd3aaa
victor bueno <victorhugomotabueno@gmail.com>
2023-09-21 11:57:14 -0300

    acesso direto as propriedades e +

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <title>Exemplo 1</title>
 </head>
 <body>
-    <h1>PHP com POO</h1>
+    <h1>PHP com POO - Exemplo 2</h1>
     <hr>
 
     <h2>Assuntos abordados: </h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importação do arquivo de Classe</li>
-        <li>Criação de instâncias/objetos</li>
+        <li>Acesso direto às propriedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de método</li>
     </ul>
 
 <?php
@@ -24,7 +24,26 @@ require_once "src/Cliente.php";
 
 $clienteA = new Cliente;
 $clienteB = new Cliente;
+
+
+// Acesso e atribuição
+$clienteA->nome = "Victor";
+$clienteB->nome = "Valeska";
+
+$clienteA->telefones = [
+    "11-94002-8922",
+    "11-92233-4455"
+];
+
+$clienteA->email = "victorhugo@gmail.com";
+$clienteA->senha = password_hash("123senac", PASSWORD_DEFAULT)
 ?>
+
+<hr>
+
+<h2>Dados dos objetos (acesso e leitura)</h2>
+<h3><?= $clienteA->nome ?></h3>
+<h3><?= $clienteB->nome ?></h3>
 
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
