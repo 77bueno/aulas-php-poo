0c731cbc37d1483261658596c3720b5ce105ff0f
victor bueno <victorhugomotabueno@gmail.com>
2023-09-22 09:13:48 -0300

    método construtor

--- index.php
+++ index.php
@@ -10,56 +10,17 @@
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Acesso direto às propriedades</li>
-        <li>Atribuição e leitura de dados</li>
-        <li>Chamada de método</li>
-        <li>Uso do <code>$this</code> para acessar recursos dentro da própria classe</li>
+        <li>Método construtor com atribuição obrigatória
+        de valores aos parâmetros/propriedades do objeto</li>
+        <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto.</li>
     </ul>
 
 <?php
 require_once "src/Cliente.php";
-
-$clienteA = new Cliente;
-$clienteB = new Cliente;
-
-// Acesso e atribuição
-$clienteA->nome = "Victor";
-$clienteB->nome = "Valeska";
-
-$clienteA->telefones = ["11-2135-0300", "11-96666-5555"];
-
-$clienteA->email = "victorh@gmail.com";
-$clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
+$clienteA = new Cliente("Ciclano", "ciclano@gmaiu");
+$clienteB = new Cliente("Clereton Savio", "teto@gmail");
 ?>
 
-<hr>
-
-<h2>Dados dos objetos (acesso e leitura)</h2>
-<h3> <?= $clienteA->nome ?> </h3>
-<p> E-mail: <?= $clienteA->email ?> </p>
-<p> Telefones (acesso um por um): 
-    <?=$clienteA->telefones[0]?>, <?=$clienteA->telefones[1]?>  
-</p>
-<p>Telefones (convertendo para string): 
-    <?=implode(", ", $clienteA->telefones)?>
-</p>
-<p>Telefones (usando loop):</p>
-<ul>
-    <?php foreach($clienteA->telefones as $telefone){ ?>
-        <li><?=$telefone?></li>
-    <?php } ?>
-</ul>
-
-
-<h2>Acessando os dados através de um método</h2>
-<?=$clienteA->exibirDados()?>
-<?=$clienteB->exibirDados()?>
-
-
-<h3> <?= $clienteB->nome ?> </h3>
-
-
-<pre><?=var_dump($clienteA, $clienteB)?></pre>
-
+<pre> <?=var_dump($clienteA, $clienteB)?> </pre>
 </body>
 </html>
