d6d9605ac053aa1afa53a3a08475b1905d8a5090
victor bueno <victorhugomotabueno@gmail.com>
2023-09-22 09:23:29 -0300

    assuntos abordados

--- index.php
+++ index.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 2</title>
+    <title>Exemplo 4</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 2</h1>
+    <h1>PHP com POO - Exemplo 4</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Método construtor com atribuição obrigatória
-        de valores aos parâmetros/propriedades do objeto</li>
-        <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto.</li>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade</li>
     </ul>
 
 <?php
