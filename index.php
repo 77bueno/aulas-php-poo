373bcdcf240d9a5c5dd4f819833edb9f748972e3
victor bueno <victorhugomotabueno@gmail.com>
2023-09-26 08:57:43 -0300

    exemplo 5 - herança

--- index.php
+++ index.php
@@ -3,35 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 4</title>
+    <title>Exemplo 5</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 4</h1>
+    <h1>PHP com POO - Exemplo 5</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Encapsulamento</li>
-        <li>Modificadores de visibilidade</li>
+        <li>Herança (Super Classe e Sub Classe)</li>
     </ul>
 
-<?php
-require_once "src/Cliente.php";
-$clienteA = new Cliente;
-
-// O código abaixo dará erro pela propriedade privada!
-// $clienteA->nome = "Thiago";
-
-// Usando o setter para atribuir um valor à propriedade privada "nome"
-$clienteA->setNome("Fulano da Silva");
-$clienteA->setEmail("fulanin@gmail");
-$clienteA->setSenha("12345bbx");
-?>
-
-<!-- Usando getter para ler/acessar dados privados -->
-<p>Nome: <?=$clienteA->getNome()?></p>
-<p>E-mail: <?=$clienteA->getEmail()?></p>
-<p>Senha: <?=$clienteA->getSenha()?></p>
-
-<pre> <?=var_dump($clienteA)?> </pre>
 </body>
 </html>
