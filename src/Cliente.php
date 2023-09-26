7c2a451bc58618c1c3910c75e09b9bb0e215b968
victor bueno <victorhugomotabueno@gmail.com>
2023-09-26 11:21:39 -0300

    Classe Abstrata

--- src/Cliente.php
+++ src/Cliente.php
@@ -1,5 +1,9 @@
 <?php 
-class Cliente {
+/* Classe Abstrata 
+Não pode ser instanciada, ou seja,
+não é possível criar um objeto a partir desta classe.
+Ela é usada apenas como modelo para subclasses com herança. */  
+abstract class Cliente {
     
     // Propriedades (ou Atributos)
     private string $nome;
