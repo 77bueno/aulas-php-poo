3571061358da7894a5d2753b33201d17b75ed5ad
victor bueno <victorhugomotabueno@gmail.com>
2023-09-29 09:28:57 -0300

    .

--- index.php
+++ index.php
@@ -21,28 +21,64 @@ já dá erro no servidor devido a terem o mesmo nome */
 
 use Fornecedor\Pagamento as FornecedorPagamento;
 use Prestador\Pagamento;
+use Tabajara\MEI;
+use Tabajara\PessoaFisica;
+use Tabajara\PessoaJuridica;
+
 
 require_once "src/fornecedores/Pagamento.php";
 require_once "src/prestadores/Pagamento.php";
 
-// Forma 1: De usar classes com namespaces
+// Forma 1 de usar classes com namespaces
 // $pagamentoFornecedor = new Fornecedor\Pagamento;
 // $pagamentoPrestador = new Prestador\Pagamento;
 
-// Forma 2: De usar classes com namespaces
+// Forma 2 de usar classes com namespaces
 // use Fornecedor\Pagamento;
-// use Prestador\Pagamento as PrestadorPagamento; // ALIAS (APELELIDO)
+// use Prestador\Pagamento as PrestadorPagamento; // ALIAS (APELIDO)
 
 // $pagamentoFornecedor = new Pagamento;
 // $pagamentoPrestador = new PrestadorPagamento; // Objeto através do alias
 
 $pagamentoFornecedor = new FornecedorPagamento;
 $pagamentoPrestador = new Pagamento;
-
 ?>
 
 <pre><?=var_dump($pagamentoFornecedor)?></pre>
 <pre><?=var_dump($pagamentoPrestador)?></pre>
 
+
+<hr>
+
+<?php 
+$clientePJ = new PessoaJuridica;
+$clientePF = new PessoaFisica;
+$MEI = new MEI;
+
+$clientePJ->setNome("Deborah Lucia");
+$clientePJ->setNomeFantasia("Deh");
+$clientePJ->setEmail("deh@gmail.com");
+
+
+$clientePF->setNome("Julia Mota");
+$clientePF->setIdade(18);
+$clientePF->setCpf("111-222-333-0");
+$clientePF->setEmail("julinha@hotmail.com");
+$clientePF->setSenha(1301);
+
+
+$MEI->setNome("Samuel José");
+$MEI->setCnpj(230203203);
+$MEI->setAnoFundacao(2015);
+$MEI->setAreaDeAtuacao("Direito - Adv");
+$MEI->setNomeFantasia("Dr. Samuel")
+?>
+
+<section>
+    <article>
+        <h2>Pessoa Jurídica</h2>
+        
+    </article>
+</section>
 </body>
 </html>
